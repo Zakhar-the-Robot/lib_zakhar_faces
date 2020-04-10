50fd52b9b2d6faef25dffe7621edf5508c4189f6
Anrei Gramakov <mail@agramakov.me>
2020-04-10 16:15:42 +0100

    moved all to the upper directory

--- include/zakhar_faces.h
+++
@@ -1,8 +0,0 @@
-#include "faces/zk_angry.h"
-#include "faces/zk_blink.h"
-#include "faces/zk_calm.h"
-#include "faces/zk_happy.h"
-#include "faces/zk_sad.h"
-
-#define FACE_WIDTH 128
-#define FACE_HEIGHT 64
