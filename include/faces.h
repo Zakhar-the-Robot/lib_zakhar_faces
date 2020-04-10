63d9da2cc88b7c4f539df21a6b15ee37c7ced1ff
an-dr <mail@agramakov.me>
2020-04-10 14:33:37 +0100

    added angry, sad, happy; re-drew calm and blink

--- include/faces.h
+++
@@ -1,5 +0,0 @@
-#include "faces/blink.h"
-#include "faces/calm.h"
-
-#define FACE_WIDTH 128
-#define FACE_HEIGHT 64
--- include/zakhar_faces.h
+++ include/zakhar_faces.h
@@ -1,5 +1,8 @@
-#include "faces/blink.h"
-#include "faces/calm.h"
+#include "faces/zk_angry.h"
+#include "faces/zk_blink.h"
+#include "faces/zk_calm.h"
+#include "faces/zk_happy.h"
+#include "faces/zk_sad.h"
 
 #define FACE_WIDTH 128
 #define FACE_HEIGHT 64
